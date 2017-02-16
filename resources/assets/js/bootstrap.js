002c6a586a18ef65158e8754d19eb5d96a78fd3b
Aleksandr Baishev <elkbullwinkle@gmail.com>
2017-02-17 10:36:49 +1300

    Switch to mix, remove dependencies, upd readme

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,5 +1,4 @@
 
-window._ = require('lodash');
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -8,7 +7,6 @@ window._ = require('lodash');
  */
 
 window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
@@ -17,31 +15,6 @@ require('bootstrap-sass');
  */
 
 window.Vue = require('vue');
-require('vue-resource');
 
-/**
- * We'll register a HTTP interceptor to attach the "CSRF" header to each of
- * the outgoing requests issued by this application. The CSRF middleware
- * included with Laravel will automatically verify the header's value.
- */
-
-Vue.http.interceptors.push((request, next) => {
-    request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
-
-    next();
-});
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
- //
- //import Echo from "laravel-echo";
- //
- //window.Echo = new Echo({
- //    broadcaster: 'pusher',
- //    key: '43c093111572d36e8b2c'
- //});
 
 
