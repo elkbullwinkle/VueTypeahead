9474d397bd3e016d1e1359315984330eb17ef8c1
Aleksandr Baishev <elkbullwinkle@gmail.com>
2017-04-11 00:15:30 +1200

    Webpack2 import issues

--- resources/assets/js/components/VueTypeahead.vue
+++ resources/assets/js/components/VueTypeahead.vue
@@ -10,7 +10,7 @@
 </template>
 <script>
   require('imports-loader?define=>false!typeahead.js/dist/typeahead.jquery.min.js');
-  var Bloodhound = require('typeahead.js')
+  var Bloodhound = require('imports-loader?define=>false!typeahead.js/dist/bloodhound.min.js');
   export default {
     data: function() {
       var id =  'typeahead-suggestion' + parseInt(Math.random() *100000);
